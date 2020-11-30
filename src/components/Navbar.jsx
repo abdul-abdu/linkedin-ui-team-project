232e62039c659184e337666f4fcc6c13d3ca4576
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-11-30 14:25:17 +0000

    fixing a name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
 
-class NavBar extends React.Component {
+class Navbar extends React.Component {
   render() {
-    return (
-      <div className="navbar">
-        <Navbar>
-          <Nav></Nav>
-        </Navbar>
-      </div>
-    );
+    return <div className="navbar"></div>;
   }
 }
 
-export default NavBar;
+export default Navbar;
