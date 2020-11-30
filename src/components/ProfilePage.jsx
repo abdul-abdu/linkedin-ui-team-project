0c9934854acbdc75867d335af544369b512a9840
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2020-11-30 16:20:59 +0100

    adds profile component

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ProfileLeft from './ProfileLeft';
+import {Container} from 'react-bootstrap';
  
 const ProfilePage = () =>{
     return(
         <>
+          <Container>
             <ProfileLeft />
+          </Container>
         </>
     );
 }
 
-export default 'ProfilePage';
+export default ProfilePage;
