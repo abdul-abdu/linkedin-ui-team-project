ac4919e6e767cc04ea16f1d65d052b6905071663
Alaa Eddine <alaneddin91@gmail.com>
2020-11-30 10:15:03 +0100

    test

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="navbar">
+        <Navbar>
+          <Nav></Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
