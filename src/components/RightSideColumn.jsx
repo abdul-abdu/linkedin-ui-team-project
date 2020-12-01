73a5e273a6972052880885f2705fa3cc515d0493
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-12-01 16:01:24 +0000

    gave PeopleRelated component props so i can change the title and position of array

--- src/components/RightSideColumn.jsx
+++ src/components/RightSideColumn.jsx
@@ -6,8 +6,12 @@ import PeopleRelated from "./PeopleRelated";
 class RightSideColumn extends React.Component {
   render() {
     return (
-      <div style={{ minWidth: "312px", position: 'relative', top: '0px' }} className='col-12 col-lg-4'>
-        <PeopleRelated />
+      <div
+        style={{ minWidth: "312px", position: "relative", top: "0px" }}
+        className="col-12 col-lg-4"
+      >
+        <PeopleRelated title="People also viewed" arrPosition={25} />
+        <PeopleRelated title="People you may know" arrPosition={5} />
         <LinkedinLearning />
       </div>
     );
