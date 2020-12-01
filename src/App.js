04287adc1b118ccc76db85afa6c6c62c98483062
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-12-01 18:03:32 +0000

    created components for other profiles and routed to them

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import NavbarApp from "./components/Navbar";
 import ProfilePage from "./components/ProfilePage";
 import { BrowserRouter, Route } from "react-router-dom";
 import Experience from "./components/Experience";
+import OtherProfile from "./components/OtherProfile";
 
 function App() {
   return (
     <BrowserRouter>
       <NavbarApp />
       <Route path="/" exact component={ProfilePage} />
+      <Route path="/profile/:id" component={OtherProfile} />
       <Route path="/experience" exact component={Experience} />
       <Footer />
     </BrowserRouter>
---
+++ src/components/OtherProfileLeft.jsx
@@ -0,0 +1,279 @@
+import React from "react";
+import Form from "../components/Form";
+import "../styles/ProfileLeft.css";
+import {
+  Button,
+  Container,
+  Row,
+  Col,
+  ProgressBar,
+  Table,
+} from "react-bootstrap";
+import { BiPencil } from "react-icons/bi";
+import { BsFillBookmarkFill } from "react-icons/bs";
+import { AiOutlinePlus } from "react-icons/ai";
+
+import DropdownProfileMenu from "./DropdownProfileMenu";
+import { withRouter } from "react-router-dom";
+
+class OtherProfileLeft extends React.Component {
+  state = {
+    user: {},
+    experiences: [],
+  };
+
+  handleClose = () => this.setState({ modalShow: false });
+  handleShow = () => this.setState({ modalShow: true });
+
+  componentDidMount = () => {
+    this.fetchProfile();
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevProps.userid !== this.props.userid) {
+      console.log(this.state.user, "this.state.user");
+
+      this.fetchExperience();
+      this.fetchProfile();
+    }
+  };
+
+  fetchProfile = async () => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/profile/" +
+          this.props.userid,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_BE_URL}`,
+          },
+        }
+      );
+      let parsedResponse = await response.json();
+      console.log(parsedResponse);
+      this.setState({ user: parsedResponse });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  fetchExperience = async () => {
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${this.props.userid}/experiences`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_BE_URL}`,
+          },
+        }
+      );
+
+      const parsedResponse = await response.json();
+      console.log(parsedResponse, "experience");
+      this.setState({ experiences: parsedResponse });
+    } catch (error) {
+      console.log("Error at experiences:", error);
+    }
+  };
+
+  render() {
+    return (
+      <div className="col-12 col-lg-8 mt-3">
+        <div className="profile-card">
+          <div
+            className="profile-profile-section"
+            style={{ maxHeight: "380px" }}
+          >
+            <div className="coverImgHolder">
+              <img
+                src="/assets/images/cover.jpg"
+                alt="cover-img"
+                className="fluid coverImg"
+              />
+            </div>
+            <div className="profilePic">
+              {this.state.user !== "" ? (
+                <img src={this.state.user.image} alt="profilePic" />
+              ) : (
+                <img
+                  src="/assets/images/user-placeholder.png"
+                  alt="profilePic"
+                />
+              )}
+            </div>
+            <div className="profile-info">
+              <div className="buttons-row">
+                <Button id="dropdown-basic-button">Message</Button>
+                <Button id="moreBtn">More...</Button>
+              </div>
+            </div>
+            <div className="nameSurnameUni">
+              {this.state.user !== "" ? (
+                <h4>
+                  {this.state.user.name} {this.state.user.surname}
+                </h4>
+              ) : (
+                <h4>Name Surname</h4>
+              )}
+              {this.state.user !== "" ? (
+                <p style={{ fontSize: "1.2rem" }}>{this.state.user.title} </p>
+              ) : (
+                <p style={{ fontSize: "1.2rem" }}>Software Engineer</p>
+              )}
+              {this.state.user !== "" ? (
+                <p style={{ lineHeight: "0.01rem" }}>
+                  {this.state.user.area} •{" "}
+                  <span style={{ color: "#0A66C2" }}>Contact info</span>
+                </p>
+              ) : (
+                <p style={{ lineHeight: "0.01rem" }}>
+                  New York •{" "}
+                  <span style={{ color: "#0A66C2" }}>Contact info</span>
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+
+        {this.state.experiences.length > 0 && (
+          <div
+            className="profile-card mt-3 profile-profile-section "
+            style={{ padding: "20px" }}
+          >
+            <Row>
+              <Col className="mb-2 pl-3">
+                <h4 onClick={() => this.props.history.push("/experience")}>
+                  Experience:{" "}
+                </h4>
+              </Col>
+            </Row>
+            {this.state.experiences.map((experience, index) => (
+              <Row className="d-flex justify-content-between" key={index}>
+                <Col xs={1}>
+                  <img src="https://placehold.it/60x60" alt="pic" />
+                </Col>
+                <Col xs={9} className="pl-4">
+                  <h6>{experience.role}</h6>
+                  <p style={{ fontSize: "0.9rem" }}>{experience.company}</p>
+                  <p style={{ fontSize: "0.7rem", marginTop: "-15px" }}>
+                    {experience.startDate}
+                  </p>
+                </Col>
+                <Button id="edit-btn"></Button>
+              </Row>
+            ))}
+          </div>
+        )}
+        <div
+          className="profile-card mt-3 profile-profile-section "
+          style={{ padding: "20px" }}
+        >
+          <Row>
+            <Col className="mb-2 pl-3">
+              <h4>Activity</h4>
+            </Col>
+            <Col>
+              <p style={{ color: "#0A66C2", float: "right" }}>See more</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} lg={6}>
+              <div className="acivity-course">
+                <Row>
+                  <Col xs={1}>
+                    <img src="https://placehold.it/60x60" alt="placeholder" />
+                  </Col>
+                  <Col xs={7} className="ml-5">
+                    <h6>Learning ECMAScript 6+ (ES6+)</h6>
+                    <p style={{ fontSize: "0.8rem" }}>shared by Name</p>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} lg={6}>
+              <div className="acivity-course">
+                <Row>
+                  <Col xs={1}>
+                    <img src="https://placehold.it/60x60" alt="placeholder" />
+                  </Col>
+                  <Col xs={7} className="ml-5">
+                    <h6>Learning ECMAScript 6+ (ES6+)</h6>
+                    <p style={{ fontSize: "0.8rem" }}>shared by Name</p>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <div
+          className="profile-card mt-3 profile-profile-section "
+          style={{ padding: "20px", maxHeight: "515px !important" }}
+        >
+          <Row>
+            <Col className="mb-2 pl-3">
+              <h4>Education: </h4>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={1}>
+              <img src="https://placehold.it/60x60" alt="placeholder" />
+            </Col>
+            <Col xs={10} className="pl-4">
+              <h6>University of Something</h6>
+              <p style={{ fontSize: "0.9rem" }}>
+                Bachelor of Computer Science, Computer Software Engineering
+              </p>
+              <p style={{ fontSize: "0.7rem", marginTop: "-15px" }}>
+                2015-2019
+              </p>
+            </Col>
+            <Col xs={1} style={{ float: "right" }}></Col>
+          </Row>
+          <hr />
+          <Row>
+            <Col className="mb-2 pl-3">
+              <h4>Licenses & certifications: </h4>
+            </Col>
+            <Col style={{ float: "right" }}></Col>
+          </Row>
+          <Row>
+            <Col xs={1}>
+              <img
+                src="/assets/images/logo.png"
+                style={{ height: "60px", width: "60px" }}
+                alt="logo"
+              />
+            </Col>
+            <Col xs={10} className="pl-4">
+              <h6>Learning ECMAScript 6+ (ES6+)</h6>
+              <p style={{ fontSize: "0.9rem" }}>LinkedIn</p>
+              <p style={{ fontSize: "0.7rem", marginTop: "-15px" }}>
+                Issued Novermber 2020 • No expiration date
+              </p>
+            </Col>
+            <Col xs={1} style={{ float: "right" }}></Col>
+          </Row>
+          <Row>
+            <Col xs={1}>
+              <img
+                src="/assets/images/logo.png"
+                style={{ height: "60px", width: "60px" }}
+                alt="logo"
+              />
+            </Col>
+            <Col xs={10} className="pl-4">
+              <h6>CSS: Selectors</h6>
+              <p style={{ fontSize: "0.9rem" }}>LinkedIn</p>
+              <p style={{ fontSize: "0.7rem", marginTop: "-15px" }}>
+                Issued Novermber 2020 • No expiration date
+              </p>
+            </Col>
+            <Col xs={1} style={{ float: "right" }}></Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(OtherProfileLeft);
