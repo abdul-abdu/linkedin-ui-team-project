6f4752734315040dae2b57d33b867f199fcca0fa
Abdugaffor-97 <abdurahimov.97@list.ru>
2020-12-02 15:14:42 +0500

    Modal implimented for creating post

--- src/components/CreateFeed.jsx
+++ src/components/CreateFeed.jsx
@@ -1,8 +1,15 @@
+import StartPost from './StartPost'
 const { Component } = require("react");
 
 class CreateFeed extends Component {
 
   render() {
-    return (<div>sss</div>)
+    return (
+      <div>
+        <StartPost />
+      </div>)
   }
 }
+
+
+export default CreateFeed
--- src/components/StartPost.jsx
+++ src/components/StartPost.jsx
@@ -5,11 +5,13 @@ import { HiOutlinePhotograph } from "react-icons/hi";
 import { AiFillPlaySquare } from "react-icons/ai";
 import { GrNotes } from "react-icons/gr";
 
+import "../styles/StartPost.css";
+
 const { Component } = require("react");
 const { Modal, Button, Form } = require("react-bootstrap");
 
 class StartPost extends Component {
-  stete = {
+  state = {
     show: false
   }
 
@@ -21,9 +23,11 @@ class StartPost extends Component {
   render() {
     return (
       <>
-        <div className='d-flex align-items-center'>
+        <div className='d-flex align-items-center'
+          onClick={this.handleShow}
+        >
           <BsPencilSquare />
-          <Form.Control size="lg" type="text" placeholder="Large text" />
+          <Form.Control size="lg" type="text" placeholder="Start a post" />
         </div>
 
         <Modal show={this.state.show} onHide={this.handleClose} animation={false}>
@@ -35,32 +39,45 @@ class StartPost extends Component {
               <Form.Control size="lg" type="textarea" placeholder="What do you want to talk about?" />
               <br />
             </Form.Group>
-            <Form.Row>
-              <Link to='#ss'>Add hashtag</Link>
-              <span>Help the right people see your post</span>
-            </Form.Row>
-            <Form.Row>
-              <div className='d-flex justify-content-between'>
-                <div>
-                  <CgMathPlus />
-                  <HiOutlinePhotograph />
-                  <AiFillPlaySquare />
-                  <GrNotes />
-                </div>
-                <div>
-                  <Button>Post</Button>
-                </div>
+            <div>
+              <Link to='#ss'>Add hashtag </Link>
+              <span> Help the right people see your post</span>
+            </div>
+            <div className='d-flex justify-content-between'>
+              <div>
+                <Link><CgMathPlus /></Link>
+                <Link><AiFillPlaySquare /></Link>
+                <Link><GrNotes /></Link>
+                <Link><HiOutlinePhotograph /></Link>
               </div>
-            </Form.Row>
+              <div>
+                <Button>Post</Button>
+              </div>
+            </div>
           </Modal.Body>
 
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Close
-          </Button>
-            <Button variant="primary" onClick={this.handleClose}>
-              Save Changes
-            </Button>
+            <div className='d-flex justify-content-between'>
+              <div>
+                <div>
+                  <Button >Celebrate an occasion</Button>
+                </div>
+                <div>
+                  <Button >Create a poll</Button>
+                </div>
+                <div>
+                  <Button >Offer help</Button>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Button >Find an export</Button>
+                </div>
+                <div>
+                  <Button >Add a profile</Button>
+                </div>
+              </div>
+            </div>
           </Modal.Footer>
         </Modal>
       </>
