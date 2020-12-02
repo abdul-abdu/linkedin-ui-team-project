3ef1349d69ef0f7bb7a07de7cff46e0185121a02
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-12-02 13:29:08 +0000

    small css and structuing

--- src/components/FeedPage.jsx
+++ src/components/FeedPage.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import PostsColumn from "./PostsColumn";
 
-import CreateFeed from './CreateFeed'
+import CreateFeed from "./CreateFeed";
 
 class FeedPage extends React.Component {
   render() {
     return (
-
       <Container className="mt-5">
         <Row>
           <Col>THIUS IS THE FEED PAGE DOOD</Col>
           <Col>
             <CreateFeed />
+            <hr />
             <PostsColumn />
           </Col>
           <Col>THIUS IS THE FEED PAGE DOOD</Col>
         </Row>
       </Container>
-
     );
   }
 }
