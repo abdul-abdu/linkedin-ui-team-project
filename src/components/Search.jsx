9c3816216f04dc2c075ab8888c6605b3af387f16
Abdugaffor-97 <abdurahimov.97@list.ru>
2020-12-04 11:47:36 +0500

    Get rid of unused imports

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import Downshift from "downshift";
 import IndividualPerson from "./IndividualPerson";
 import "./styles/Navbar.css";
@@ -20,14 +19,14 @@ class Search extends Component {
     if (!event.target.value) {
       return;
     }
-    else{
-        // this.fetchUsers(event.target.value);
-        console.log('before setState: ', this.state.users);
-        
-        // this.setState({users: ['whatever']});
-        console.log('after setState: ', this.state.users);
-      await   this.fetchUsers()
-        this.setState({users: this.state.users.filter(user => user.name && user.name.toLowerCase().includes(event.target.value))});
+    else {
+      // this.fetchUsers(event.target.value);
+      console.log('before setState: ', this.state.users);
+
+      // this.setState({users: ['whatever']});
+      console.log('after setState: ', this.state.users);
+      await this.fetchUsers()
+      this.setState({ users: this.state.users.filter(user => user.name && user.name.toLowerCase().includes(event.target.value)) });
     }
   }
 
@@ -35,16 +34,16 @@ class Search extends Component {
     // alert(`you have selected ${selectedUser.name} ${selectedUser.surname}`);
   }
 
-  componentDidMount = () =>{
-      this.fetchUsers();
+  componentDidMount = () => {
+    this.fetchUsers();
   }
 
-//   fetchMovies(movie) {
-//     const moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=${movie}`;
-//     axios.get(moviesURL).then(response => {
-//       this.setState({ movies: response.data.results });
-//     });
-//   }
+  //   fetchMovies(movie) {
+  //     const moviesURL = `https://api.themoviedb.org/3/search/movie?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=${movie}`;
+  //     axios.get(moviesURL).then(response => {
+  //       this.setState({ movies: response.data.results });
+  //     });
+  //   }
 
   fetchUsers = async () => {
     try {
@@ -56,8 +55,8 @@ class Search extends Component {
           },
         });
       let parsedResponse = await response.json();
-     // console.log('parsedRes::::::::', parsedResponse);
-    //   profiles.push(parsedResponse);
+      // console.log('parsedRes::::::::', parsedResponse);
+      //   profiles.push(parsedResponse);
       this.setState({ users: parsedResponse })
     } catch (error) {
       console.log(error);
@@ -79,51 +78,51 @@ class Search extends Component {
           inputValue,
           getLabelProps
         }) => (
-          <div>
-           {/* <label
+            <div>
+              {/* <label
               style={{ marginTop: "1rem", display: "block" }}
               {...getLabelProps()}
             >
               Search for users
         </label>{" "}*/}
-            <br />
-            <input class='search-btn'
-              {...getInputProps({
-                placeholder: "Search users",
-                onChange: this.inputOnChange
-              })}
-            />
-            {isOpen ? (
-              <div className="downshift-dropdown">
-             
-                 {this.state.users
-                
-                  .slice(0, 5)
-                  .map((item, index) => (
-                    <div
-                      className="dropdown-item"
-                      {...getItemProps({ key: index, index, item })}
-                      style={{
-                        backgroundColor:
-                          highlightedIndex === index ? "lightgray" : "white",
-                        fontWeight: selectedItem === item ? "bold" : "normal"
-                      }}
-                    >
-                      {item.name && item.surname && 
-                        <IndividualPerson style={{fontSize: 'smaller'}}
-                  name={item.name}
-                  job={item.title}
-                  pic={item.image}
-                  userid={item._id}
-                  divider={index === 4 ? false : true}
-                />}
+              <br />
+              <input class='search-btn'
+                {...getInputProps({
+                  placeholder: "Search users",
+                  onChange: this.inputOnChange
+                })}
+              />
+              {isOpen ? (
+                <div className="downshift-dropdown">
+
+                  {this.state.users
+
+                    .slice(0, 5)
+                    .map((item, index) => (
+                      <div
+                        className="dropdown-item"
+                        {...getItemProps({ key: index, index, item })}
+                        style={{
+                          backgroundColor:
+                            highlightedIndex === index ? "lightgray" : "white",
+                          fontWeight: selectedItem === item ? "bold" : "normal"
+                        }}
+                      >
+                        {item.name && item.surname &&
+                          <IndividualPerson style={{ fontSize: 'smaller' }}
+                            name={item.name}
+                            job={item.title}
+                            pic={item.image}
+                            userid={item._id}
+                            divider={index === 4 ? false : true}
+                          />}
                         {/* item.name + ' '+ item.surname*/}
-                    </div>
-              ))}
-              </div>
-            ) : null}
-          </div>
-        )}
+                      </div>
+                    ))}
+                </div>
+              ) : null}
+            </div>
+          )}
       </Downshift>
     );
   }
